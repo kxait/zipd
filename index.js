1dfa716af5c1bd55fc5ce50ffd37838b1001737a
kxait <0kxait@gmail.com>
2019-11-30 18:06:49 +0100

    fixed port

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ const jsonParser = bodyParser.json();
 const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.render("login.ejs");
